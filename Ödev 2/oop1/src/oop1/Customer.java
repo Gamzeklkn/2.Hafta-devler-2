91f058914930906997cce850818be0646d5ea260
Gamzeklkn <114660777+Gamzeklkn@users.noreply.github.com>
2022-10-11 14:24:33 +0300

    Add files via upload

---
+++ Ödev 2/oop1/src/oop1/Customer.java
@@ -0,0 +1,32 @@
+package oop1;
+
+public class Customer {
+	private int id;
+	private String customerNamber;
+	private String phone;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCustomerNamber() {
+		return customerNamber;
+	}
+
+	public void setCustomerNamber(String customerNamber) {
+		this.customerNamber = customerNamber;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+}
